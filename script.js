79a0a60ac3144971e56c2474d2bd99253e51a668
Cuong Tran <amt.freelance@gmail.com>
2021-11-23 15:31:08 +0700

    update ui for rps game

--- script.js
+++ script.js
@@ -1,50 +1,77 @@
-const selection = ["rock", "paper", "scissors"];
+// Get list of weapons
+const selection = document.querySelectorAll("button");
+
+//Get random weapon for computer
 function computerPlay() {
-  return selection[Math.floor(Math.random() * 3)];
+  return selection[Math.floor(Math.random() * 3)].innerText.toLowerCase();
 }
+
 let computerSelection;
 let playerSelection;
 let yourPoints = 0;
 let computerPoints = 0;
+let click = 0;
+const roundResultList = document.getElementById("#round-result-list");
 function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt("Your selection?").toLowerCase();
+  playerSelection = this.innerText.toLowerCase();
   computerSelection = computerPlay();
+  click += 1;
+  let li = document.createElement("li");
+  // There are 7 cases can happen when you and computer chose your own weapon
   if (playerSelection === computerSelection) {
-    console.log(`Tie! Both of you choose ${playerSelection}`);
+    li.innerHTML = `Tie! Both of you choose ${playerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     yourPoints += 1;
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    li.innerHTML = `You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerPoints += 1;
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    li.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerPoints += 1;
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    li.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     yourPoints += 1;
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    li.innerHTML = `You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerPoints += 1;
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    li.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     yourPoints += 1;
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    li.innerHTML = `You Win! ${playerSelection} beats ${computerSelection}`;
   }
+
+  if (click <= 5) {
+    roundResultList.appendChild(li);
+  }
+  // count on rounds
+  if (click === 5) {
+    let annoucementText;
+    if (yourPoints > computerPoints) {
+      annoucementText = "Congrats! You are the champion";
+    } else if (yourPoints < computerPoints) {
+      annoucementText = "You lose, computer is the winner";
+    } else {
+      annoucementText = "Tie!";
+    }
+    document.querySelector(".annoucement").innerHTML =
+      "<h2>" + annoucementText + " after 5 rounds </h2>";
+  }
+
+  if (click > 5) {
+    click = 0;
+    yourPoints = 0;
+    computerPoints = 0;
+    document.querySelector(".annoucement").innerHTML = "";
+    roundResultList.innerHTML = "";
+    window.addEventListener("click", () => {
+      location.reload();
+    });
+  }
+  document.querySelector(".yourscore").innerHTML = yourPoints;
+  document.querySelector(".computerscore").innerHTML = computerPoints;
 }
+
 function game() {
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  if (yourPoints > computerPoints) {
-    console.log(
-      `You: ${yourPoints}\nComputer: ${computerPoints}.\nYou are the champion`
-    );
-  } else if (yourPoints < computerPoints) {
-    console.log(`You: ${yourPoints}\nComputer: ${computerPoints}\nYou lose!`);
-  } else {
-    console.log(`You: ${yourPoints}\nComputer: ${computerPoints}\nTie!`);
-  }
+  selection.forEach((btn) => btn.addEventListener("click", playRound));
 }
 game();
